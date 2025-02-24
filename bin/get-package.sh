d9b5bd655c04735abfe5fcdcbcebbd08aecf558a
Dvir <dvir5049@gmail.com>
2025-02-24 08:26:05 -0500

    Add script to get package name

--- bin/get-package.sh
+++ bin/get-package.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env bash
 
+set -e
+
+version=$(grep "__title__ = " spacy/about.py)
+version=${version/__title__ = }
+version=${version/\'/}
+version=${version/\'/}
+version=${version/\"/}
+version=${version/\"/}
+
+echo $version
